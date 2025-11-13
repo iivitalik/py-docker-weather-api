873ab05d84bb264640e52063306b83742ad5f1f0
Vitalik <lg.mobile0808@gmail.com>
2025-11-13 19:07:01 +0200

    docker solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
+import os, requests
+
+
+key = os.getenv("API_KEY")
+
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    response = requests.get(
+        "http://api.weatherapi.com/v1/current.json",
+        params={"key": key, "q": "Paris"}
+    )
+
+    data = response.json()
+
+    print(f"Current weather in {data["location"]["name"]} is "
+          f"{data["current"]["temp_c"]}°C")
 
 
 if __name__ == "__main__":
